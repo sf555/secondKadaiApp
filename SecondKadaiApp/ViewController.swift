572c2e22288dcad51ce522cbc700a5e549b6d205
FUKO SUZUKI <test@test-no-MacBook-Pro.local>
2019-04-04 21:20:18 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by FUKO SUZUKI on 2019/04/04.
+//  Copyright © 2019 FUKO.SUZUKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
